a5cb516d5f249dfbbc97ccc67d9f461fd1db47e4
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-08-07 21:51:55 +0100

    FIX: Away team working with sortable order

--- src/Model/Match.php
+++ src/Model/Match.php
@@ -6,11 +6,17 @@ use SilverStripe\Assets\Image;
 use SilverStripe\Forms\DatetimeField;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldConfig;
 use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\TextareaField;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\View\HTML;
+use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 class Match extends DataObject
 {
@@ -33,8 +39,22 @@ class Match extends DataObject
         'Innings' => Innings::class
     ];
 
+    private static $many_many = [
+        'HomeTeamPlayers' => Player::class,
+        'AwayTeamPlayers' => Player::class
+    ];
+
+    private static $many_many_extraFields = [
+        'HomeTeamPlayers' => [
+            'SortOrder' => 'Int',
+        ],
+        'AwayTeamPlayers' => [
+            'SortOrder' => 'Int',
+        ],
+    ];
+
     private static $belongs_to = [
-        MatchReport::class
+        'MatchReport' => MatchReport::class
     ];
 
     private static $summary_fields = [
@@ -48,6 +68,10 @@ class Match extends DataObject
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
+
+        // remove scaffolded fields
+        $fields->removeByName('HomeTeamPlayers');
+        $fields->removeByName('AwayTeamPlayers');
 
         // result, when,status,ground,home team, away team, competition fixed
 
@@ -67,10 +91,51 @@ class Match extends DataObject
         map('ID', 'Title')) ->setEmptyString('-- Select home team --');
         $fields->addFieldToTab('Root.Main', $homeTeamField);
 
+        /*
+        if ($this->HomeTeamID) {
+            $confHome = GridFieldConfig_RelationEditor::create(20);
+            $confHome->removeComponentsByType(GridFieldAddNewButton::class);
+
+            // @todo This is not working
+            //$conf->addComponent($sortable = new GridFieldSortableRows('SortOrder'));
+            //$sortable->setCustomRelationName('HomeTeamPlayers');
+            $teamGridHome = GridField::create(
+                'HomeTeamPlayers',
+                'Home Players',
+                $this->HomeTeamPlayers(),
+                $confHome
+            );
+
+            $fields->addFieldToTab('Root.HomeTeam', $teamGridHome);
+        }
+        */
+
         $awayTeamField = DropdownField::create('AwayTeamID', 'Away Team', Team::get()->
         sort('Name')->
         map('ID', 'Title')) ->setEmptyString('-- Select away team --');
         $fields->addFieldToTab('Root.Main', $awayTeamField);
+
+        if ($this->AwayTeamID) {
+            $confAway = GridFieldConfig_RelationEditor::create(20);
+            $confAway->removeComponentsByType(GridFieldAddNewButton::class);
+
+            $autoCompleteComponent = $confAway->getComponentByType(GridFieldAddExistingAutocompleter::class);
+            $autoCompleteComponent->setResultsFormat('$Surname, $FirstName');
+
+
+
+            // @todo This does not work
+            $confAway->addComponent($sortable = new GridFieldSortableRows('SortOrder'));
+            //$sortable->setCustomRelationName('AwayTeanPlayers');
+            $teamGridAway = GridField::create(
+                'AwayTeamPlayers',
+                'Away Players',
+                $this->AwayTeamPlayers(),
+                $confAway
+            );
+
+            $fields->addFieldToTab('Root.AwayTeam', $teamGridAway);
+        }
 
         $fields->addFieldToTab('Root.Main', TextareaField::create(
             'Result',
@@ -121,6 +186,17 @@ class Match extends DataObject
         }
 
         return $result;
+    }
+
+
+    public function HomeTeamPlayers()
+    {
+        return $this->getManyManyComponents('HomeTeamPlayers')->sort('SortOrder');
+    }
+
+    public function AwayTeamPlayers()
+    {
+        return $this->getManyManyComponents('AwayTeamPlayers')->sort('SortOrder');
     }
 
 }
