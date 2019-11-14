12200704b44fbf484540f8098b7847268837ff0a
Gordon Anderson <gordon.b.anderson@gmail.com>
2019-11-14 20:55:40 +0000

    WIP: Fielding players on a scorecard now imported

--- src/Helper/ImportScorecardHelper.php
+++ src/Helper/ImportScorecardHelper.php
@@ -2,15 +2,13 @@
 
 namespace Suilven\CricketSite\Helper;
 
-use Intervention\Image\Image;
-use Intervention\Image\ImageManager;
 use PhpOffice\PhpSpreadsheet\Cell\Cell;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use SilverStripe\Assets\Storage\DBFile;
-use SilverStripe\ORM\ArrayList;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\SiteConfig\SiteConfig;
 use Suilven\CricketSite\Model\Club;
+use Suilven\CricketSite\Model\HowOut;
+use Suilven\CricketSite\Model\InningsBattingEntry;
 use Suilven\CricketSite\Model\Player;
 use Suilven\CricketSite\Model\Team;
 use Suilven\Sluggable\Helper\SluggableHelper;
@@ -32,7 +30,7 @@ class ImportScorecardHelper
         $this->parseClubAndTeams($spreadsheet);
 
 
-        $this->parseInnings($spreadsheet);
+        $this->parseAllInnings($spreadsheet);
 
 
     }
@@ -85,18 +83,23 @@ class ImportScorecardHelper
         return $team;
     }
 
-    private function createOrGetPlayer($club, $fieldValue)
+    private function createOrGetPlayer($team, $fieldValue)
     {
         $splits = explode(' ', $fieldValue);
         $params = [];
 
         // this logic may not work for some asian names, these will need fixed by hand
         $params['FirstName'] = $splits[0];
-        if (sizeof($splits > 1)) {
+        $params['DisplayName'] = $splits[0];
+        if (sizeof($splits ) > 1) {
             $params['Surname'] = $splits[1];
+            $params['DisplayName'] = $splits[0] . ' ' . $splits[1];
         }
 
         $player = $this->createOrGetBySlug(Player::class, $fieldValue, 'DisplayName', $params);
+
+        // this does not create multiple duplicates, if the relationship exists another is not added
+        $player->Clubs()->add($team->Club());
         $player->write();
 
         //
@@ -140,30 +143,62 @@ class ImportScorecardHelper
      * @param \PhpOffice\PhpSpreadsheet\Spreadsheet $spreadsheet
      * @throws \PhpOffice\PhpSpreadsheet\Exception
      */
-    public function parseInnings(\PhpOffice\PhpSpreadsheet\Spreadsheet $spreadsheet)
+    public function parseAllInnings(\PhpOffice\PhpSpreadsheet\Spreadsheet $spreadsheet)
     {
-        $sheet = $spreadsheet->getSheet(1);
+        $this->parseInnings($spreadsheet->getSheet(1));
+        //$this->parseInnings($spreadsheet->getSheet(2));
+    }
+
+    /**
+     * @param \PhpOffice\PhpSpreadsheet\Spreadsheet $spreadsheet
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function parseInnings(Worksheet $sheet)
+    {
         $battingClubName = $sheet->getCell('B1')->getCalculatedValue();
         $slugHelper = new SluggableHelper();
         $slug = $slugHelper->getSlug($battingClubName);
         $teamBatting = null;
-        switch($slug)
-        {
+        $teamBowling = null;
+        switch ($slug) {
             case $this->homeClub->Slug:
                 $teamBatting = $this->homeTeam;
+                $teamBowling = $this->awayTeam;
                 break;
             case $this->awayClub->Slug:
                 $teamBatting = $this->awayTeam;
+                $teamBowling = $this->homeTeam;
                 break;
             default:
                 user_error('The team batting could not be determined from the value ' . $battingClubName);
                 break;
         }
 
-        for ($i =4; $i <=14; $i++) {
+        for ($i = 4; $i <= 14; $i++) {
             $playerName = $sheet->getCell('A' . $i)->getCalculatedValue();
             error_log($playerName);
             $this->createOrGetPlayer($teamBatting, $playerName);
+
+            $howoutShortTitle1 = $sheet->getCell('B' . $i);
+            $howOut1 = HowOut::get()->filter(['ShortTitle' => $howoutShortTitle1])->first();
+            $fielder1Name = $sheet->getCell('C' . $i);
+            $fielder1 = null;
+            error_log('T1 fielder 1 name = *' . $fielder1Name . '*');
+            if (strlen($fielder1Name) > 0) {
+                error_log('NOT EMPTY!!!!');
+                $fielder1 = $this->createOrGetPlayer($teamBowling, $fielder1Name);
+            }
+
+            $howoutShortTitle2 = $sheet->getCell('D' . $i);
+            $howOut2 = HowOut::get()->filter(['ShortTitle' => $howoutShortTitle2])->first();
+            $fielder2Name = $sheet->getCell('E' . $i);
+            $fielder2 = null;
+            if (strlen($fielder2Name) > 0) {
+                $fielder2 = $this->createOrGetPlayer($teamBowling, $fielder2Name);
+            }
+            error_log($fielder2Name);
+            $inningsEntry = new InningsBattingEntry();
+
 
         }
     }
